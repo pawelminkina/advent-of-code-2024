682bd8a6ef309f2bba90cd48667ab00868fc7d0d
pavixonpl <pawmin14@gmail.com>
2024-12-27 12:23:47 +0100

    good answer

--- day-2/day2.go
+++ day-2/day2.go
@@ -9,7 +9,7 @@ import "strconv"
 
 
 func main() {
-    file, err := os.Open("testinput.txt")
+    file, err := os.Open("input.txt")
     if err != nil {
         log.Fatal(err)
     }
@@ -19,77 +19,23 @@ func main() {
     var safeReports = 0
     for scanner.Scan() {
         var text = scanner.Text()
-        fmt.Println("current report: ", text)
         var itemsInOneLine = strings.Split(text, " ")
-        var lastNumber = math.MinInt
-        var isSafe = false
-        var alreadyFailedForLine = false
-        var processedNumbers []int
-        for i := 0; i < len(itemsInOneLine); i++ {
-            currentNumber, _ := strconv.Atoi(itemsInOneLine[i])
-            var isIncreasing = IsIncreasingFunc(currentNumber, lastNumber) 
-            var isDecreasing = IsDecreasingFunc(currentNumber, lastNumber)
-            if i > 0 && !isIncreasing && !isDecreasing {
-                if alreadyFailedForLine  {
-                    fmt.Println("Failed line: ", text)
-                    isSafe = false
-                    break
-                }
-                if len(processedNumbers) > 2 && !IsIncreasingFunc(currentNumber, processedNumbers[i-2]) && !IsDecreasingFunc(currentNumber, processedNumbers[i-2]) {
-                    fmt.Println("Happened here for text: ", text)
-                    alreadyFailedForLine = true
-                    continue
-                }
-                if (len(itemsInOneLine) > 2 && i == 1){
-                    nextItem, _ := strconv.Atoi(itemsInOneLine[i+1])
-                    if IsIncreasingFunc(nextItem, currentNumber) || IsDecreasingFunc(nextItem, currentNumber){
-                        alreadyFailedForLine = true
-
-                        processedNumbers = append(processedNumbers, currentNumber)
-                        lastNumber = currentNumber
-                        isSafe = true
-                        continue
-                    }
-                } 
-                alreadyFailedForLine = true
-                continue
-            }
-          processedNumbers = append(processedNumbers, currentNumber)
-          lastNumber = currentNumber
-          isSafe = true
-          if i > 0 && !(areAllDecreasing(processedNumbers) || areAllIncreasing(processedNumbers)) {
-
-                fmt.Println("failed for number : ", currentNumber)
-                fmt.Println("in text: ", strings.Trim(strings.Join(strings.Fields(fmt.Sprint(processedNumbers)), ","), "[]"))
-            if alreadyFailedForLine {
-                fmt.Println("Unsafe report, isIncreasing: ", text)
-                isSafe = false
-                break
-                }
-            if len(processedNumbers) == 3 {
-                var newNumbers []int 
-                    for j := 1; j < len(itemsInOneLine); j++ {
-                        val, _ := strconv.Atoi(itemsInOneLine[j])
-                        newNumbers = append(newNumbers, val)
-                    }
-                    fmt.Println("newnumbers: ", strings.Trim(strings.Join(strings.Fields(fmt.Sprint(newNumbers)), ","), "[]"))
-                    var removingFirstWorks = areAllDecreasing(newNumbers) || areAllIncreasing(newNumbers)
-                fmt.Println("removing first works: ", removingFirstWorks)
-                    if (removingFirstWorks) {
-                        processedNumbers = processedNumbers[1:] 
-                fmt.Println("fater removal: ", processedNumbers)
-                        alreadyFailedForLine = true
-                        continue
-                    }
-                }
-            processedNumbers = processedNumbers[:len(processedNumbers)-1]
-            processedNumbers[i-1] = currentNumber
-            alreadyFailedForLine = true
-          }
-        }
-        if isSafe && len(itemsInOneLine) > 1 {
+        fmt.Println("current report: ", text)
+        var isSafe = IsSafe(itemsInOneLine)
+        if isSafe {
             fmt.Println("Safe report: ", text)
             safeReports++
+        } else {
+            for i := 0; i < len(itemsInOneLine); i++ {
+            //w petli chce zwyczjanie robic remove at
+                var slicedText = RemoveIndex(itemsInOneLine, i)
+                isSafe = IsSafe(slicedText)
+                if isSafe {
+                    fmt.Println("Safe report: ", text)
+                    safeReports++
+                    break
+                }
+            }
         }
     }
     fmt.Println("Safe reports: ", safeReports) 
@@ -99,6 +45,31 @@ func main() {
     }
 }
 
+func RemoveIndex(s []string, index int) []string {
+    ret := make([]string, 0)
+    ret = append(ret, s[:index]...)
+    return append(ret, s[index+1:]...)
+}
+
+func IsSafe(itemsInOneLine []string) bool {
+    var processedNumbers []int
+    var lastNumber = math.MinInt
+    for i := 0; i < len(itemsInOneLine); i++ {
+            currentNumber, _ := strconv.Atoi(itemsInOneLine[i])
+            var isIncreasing = IsIncreasingFunc(currentNumber, lastNumber) 
+            var isDecreasing = IsDecreasingFunc(currentNumber, lastNumber)
+            if i > 0 && !isIncreasing && !isDecreasing {
+                return false
+            }
+    processedNumbers = append(processedNumbers, currentNumber)
+    lastNumber = currentNumber
+    }
+ 
+    if !(areAllDecreasing(processedNumbers) || areAllIncreasing(processedNumbers)) {
+        return false
+    }
+    return true
+}
 func IsIncreasingFunc(currentNumber int, lastNumber int) bool{
             return currentNumber  >= lastNumber + 1 && currentNumber <= lastNumber + 3
 }
